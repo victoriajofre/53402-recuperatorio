9b1e7069f87435d3d1d621d6d194d8a9232b19c2
victoriajofre <victoriajofre080306@gmail.com>
2026-06-11 15:23:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import antlr4 from "antlr4";
 import fs from "fs";
 
-import ConteoLexer from "./generated/ConteoLexer.js";
-import ConteoParser from "./generated/ConteoParser.js";
+import ConteoLexer from "./ConteoLexer.js";
+import ConteoParser from "./ConteoParser.js";
 import CustomConteoVisitor from "./CustomConteoVisitor.js";
-const input = fs.readFileSync("input.txt", "utf8");
+const archivo = process.argv[2] || "input.txt";
+const input = fs.readFileSync(archivo, "utf8");
 
 const chars = new antlr4.InputStream(input);
 const lexer = new ConteoLexer(chars);
@@ -30,6 +31,10 @@ const parser = new ConteoParser(tokenStream);
 parser.buildParseTrees = true;
 
 const tree = parser.programa();
+if (parser._syntaxErrors > 0) {
+    console.log("\n===== SE ENCONTRARON ERRORES SINTACTICOS =====");
+    process.exit(1);
+}
 
 console.log("\n===== ARBOL SINTACTICO =====");
 console.log(tree.toStringTree(parser.ruleNames));
